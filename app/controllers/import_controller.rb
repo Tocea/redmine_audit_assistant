ef78c1594d04ff67969c690c3ca77c0c782bea71
dmichel <david.michel@tocea.com>
2015-04-15 15:58:41 +0200

    display an error message if the file cannot be parsed #2

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -14,32 +14,46 @@ class ImportController < ApplicationController
     @versions.each do |v|
       @select_versions.push([v.name, v.id])
     end
-
+    
+    if params[:error_parsing_file]
+      flash[:error] = l(:error_parsing_file)
+    end   
+    
   end
 
   def import
     
     # retrieve the project
     @project = Project.find(params[:project_id])
-    puts @project
     
     # retrieve the version
     version_id = params[:version_id]
-    puts version_id
     if (version_id && version_id != '0')
       @version = Version.find(version_id)
     end
-    puts @version.inspect
     
-    # retrieve the attachment
+    # redirect to the index page if no attachment has been uploaded
     attachments = params[:attachments]
-    puts attachments[attachments.keys[0]]["token"]
-    token = attachments[attachments.keys[0]]["token"]
-    attach = Attachment.find_by_token(token)
-    puts attach
+    token = attachments[attachments.keys[0]]["token"]   
+    if !token
+      Rails.logger.info "No attachment found!"
+      redirect_to :controller => 'import', :action => 'index', :project_id => @project.id
+      return false
+    end
     
-    # convert the attachment to Requirement objects
-    requirements = ImportHelper::import_from_yaml(attach.diskfile)
+    # retrieve the attachment   
+    attach = Attachment.find_by_token(token)
+    
+    begin     
+      # convert the attachment to Requirement objects
+      requirements = ImportHelper::import_from_yaml(attach.diskfile)  
+    rescue      
+      # redirect to the index page if the file cannot be parsed
+      Rails.logger.info "Error parsing file"
+      redirect_to :controller => 'import', :action => 'index', :project_id => @project.id, :error_parsing_file => true
+      return false
+    end
+    
     
     # create issues from the requirements
     requirements.each do |r|
