b9ba4dd325deabab775c614a27615c3e84135df7
dmichel <david.michel@tocea.com>
2015-05-27 10:47:34 +0200

    fixed travis build failure when trying to get the max value of an array that contains Date objects and nil values

--- app/models/project_progress_report.rb
+++ app/models/project_progress_report.rb
@@ -11,10 +11,11 @@ class ProjectProgressReport < ProgressReport
   
   def date_effective
     vArr = versions
+    vArr = vArr.reject { |v| v.effective_date.nil? } 
     if vArr.blank?
       return super
-    end   
-    vArr.compact.map { |v| v.effective_date }.max
+    end 
+    vArr.map { |v| v.effective_date }.max
   end
   
 end
